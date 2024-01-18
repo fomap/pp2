16735fcc39712c0c435fe6def5dee243d0943325
Beka <fomapking@gmail.com>
2024-01-18 23:08:37 +0600

    Initial commit

---
+++ lab1/pythonComment.py
@@ -0,0 +1,7 @@
+"""
+This is a comment
+written in
+more than just one line
+"""
+#This is also a comment
+print("Hello, World!")
---
+++ lab1/pythonDataTypes.py
@@ -0,0 +1,13 @@
+x = 5
+print(type(x))
+
+x = 1j
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#specifying data types
+x = int(20)
+x = complex(1j)
+x = dict(name="John", age=36)
