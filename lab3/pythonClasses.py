ff7d7d717c781636eee4b1ae32156253f231aa8e
Beka <anellbektassova@gmail.com>
2024-02-09 23:40:20 +0600

    classes beginning

---
+++ lab3/pythonClasses.py
@@ -0,0 +1,23 @@
+class Shape:
+    def __init__(self, lengthParam):
+        self.length = lengthParam
+
+    def getString(self):
+        print("Enter value: ")
+        invalue = input()
+        # print(invalue)
+    
+    def printString(self):
+        print("Enter str")
+        str = input()
+        str2 = str.upper()
+        print(str2)
+
+    # def area(self):
+
+    
+
+
+beka = Shape("15")
+# lalala = input("enter")
+beka.printString()
