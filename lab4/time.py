61a93e10a3a7d9f59f23c59a3be9be23d6843a97
Beka <anellbektassova@gmail.com>
2024-02-24 20:58:43 +0600

    final version

--- lab4/time.py
+++ lab4/time.py
@@ -1,39 +1,46 @@
+'''
 import datetime
-# from datetime import datetime, date, time
-
-# def backToPast(n):
-#     return datetime.date.today() - datetime.timedelta(n)
-
-# value = int(input("enter how far to go back"))
-# print(backToPast(value))
 
 
-# def threeDays():
-#     yesterday = datetime.date.today() - datetime.timedelta(1)
-#     today = datetime.date.today()
-#     tomorrow = datetime.date.today() + datetime.timedelta(1)
-#     return yesterday, today, tomorrow
+1)
+def backToPast(n):
+    return datetime.date.today() - datetime.timedelta(n)
 
-# print(threeDays())
-
-
-# def dropMilliseconds():
-#     rightNow = datetime.datetime.now()
-#     formatted = rightNow.strftime("%m/%d/%Y, %H:%M:%S")
-#     return formatted
-
-# print(dropMilliseconds())
+value = int(input("enter how far to go back"))
+print(backToPast(value))
 
 
-def difference(str1, str2):
-    day1_object = datetime.datetime.strptime(str1, '%m/%d/%y %H:%M:%S')
-    day2_object = datetime.datetime.strptime(str2, '%m/%d/%y %H:%M:%S')
+
+2)
+def threeDays():
+    yesterday = datetime.date.today() - datetime.timedelta(1)
+    today = datetime.date.today()
+    tomorrow = datetime.date.today() + datetime.timedelta(1)
+    return yesterday, today, tomorrow
+
+print(threeDays())
+
+3)
+
+def dropMilliseconds():
+    rightNow = datetime.datetime.now()
+    formatted = rightNow.strftime("%m/%d/%Y, %H:%M:%S")
+    return formatted
+
+print(dropMilliseconds())
+'''
+
+
+from datetime import datetime
+
+
+def diffFunction(str1, str2):
+    day1_object = datetime.strptime(str1, '%Y-%m-%d %H:%M:%S')
+    day2_object = datetime.strptime(str2, '%Y-%m-%d %H:%M:%S')
     diff = day1_object - day2_object
-    return diff.second()
+    return diff.total_seconds()
 
 
-day1 = input("Please enter in mm/dd/yyyy HOURS:MINUTES:SECONDS format ")
-
-day2 = input("Please enter in mm/dd/yyyy HOURS:MINUTES:SECONDS format ")
-
-print(difference(day1, day2))
+day1 = input("Please enter in YYYY-MM-DD HH:MM:SS format ")
+day2 = input("Please enter in YYYY-MM-DD HH:MM:SS format ")
+print(diffFunction(day1, day2))
