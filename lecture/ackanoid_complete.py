58625d3d79adfa7f7fc7b35ce9e90d4b19e70df5
Beka <anellbektassova@gmail.com>
2024-03-22 11:41:07 +0500

    lecture material

---
+++ lecture/ackanoid_complete.py
@@ -0,0 +1,115 @@
+import pygame 
+import random
+pygame.init()
+
+W, H = 1200, 800
+FPS = 60
+
+screen = pygame.display.set_mode((W, H), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+done = False
+bg = (255, 192, 203)
+
+#paddle
+paddleW = 150
+paddleH = 25
+paddleSpeed = 20
+paddle = pygame.Rect(W // 2 - paddleW // 2, H - paddleH - 30, paddleW, paddleH)
+
+
+#Ball
+ballRadius = 20
+ballSpeed = 6
+ball_rect = int(ballRadius * 2 ** 0.5)
+ball = pygame.Rect(random.randrange(ball_rect, W - ball_rect), H // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
+
+#block settings
+block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range (4)]
+color_list = [(random.randrange(150, 256), random.randrange(150, 256),  random.randrange(150, 256)) for i in range(10) for j in range(4)] 
+
+#Game over Screen
+losefont = pygame.font.SysFont('comicsansms', 40)
+losetext = losefont.render('Game Over', True, (255, 255, 255))
+losetextRect = losetext.get_rect()
+losetextRect.center = (W // 2, H // 2)
+
+#Win Screen
+winfont = pygame.font.SysFont('comicsansms', 40)
+wintext = losefont.render('You win yay', True, (255, 255, 255))
+wintextRect = wintext.get_rect()
+wintextRect.center = (W // 2, H // 2)
+
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    screen.fill(bg)
+    
+
+    [pygame.draw.rect(screen, color_list[color], block) for color, block in enumerate (block_list)] #drawing blocks
+    pygame.draw.rect(screen, pygame.Color(234, 250, 177), paddle)
+    pygame.draw.circle(screen, pygame.Color(250, 241, 157), ball.center, ballRadius)
+    
+
+    #Ball movement
+    ball.x += ballSpeed * dx
+    ball.y += ballSpeed * dy
+
+    #Collision left 
+    if ball.centerx < ballRadius or ball.centerx > W - ballRadius:
+        dx = -dx
+    #Collision right
+    if ball.centery < ballRadius: 
+        dy = -dy
+    #Collision with paddle
+    if ball.colliderect(paddle) and dy > 0:
+        dx, dy = detect_collision(dx, dy, ball, paddle)
+    #Collision blocks
+    hitIndex = ball.collidelist(block_list)
+
+    if hitIndex != -1:
+        hitRect = block_list.pop(hitIndex)
+        hitColor = color_list.pop(hitIndex)
+        dx, dy = detect_collision(dx, dy, ball, hitRect)
+    
+    #Win/lose screens
+    if ball.bottom > H:
+        screen.fill((0, 0, 0))
+        screen.blit(losetext, losetextRect)
+    elif not len(block_list):
+        screen.fill((255,255, 255))
+        screen.blit(losetext, losetextRect)
+
+    #Paddle Control
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddleSpeed
+    if key[pygame.K_RIGHT] and paddle.right < W:
+        paddle.right += paddleSpeed
+
+
+    pygame.display.flip()
+    clock.tick(FPS)
